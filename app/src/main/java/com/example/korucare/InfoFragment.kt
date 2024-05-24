9c09c414492cea8be8dd8ae491ada2237b3f6747
zihan <2411663991@qq.com>
2024-05-25 03:02:53 +1200

    adjust top logo

---
+++ app/src/main/java/com/example/korucare/InfoFragment.kt
@@ -0,0 +1,11 @@
+package com.example.korucare
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class InfoFragment : Fragment(R.layout.fragment_info) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/korucare/MainActivity.kt
+++ app/src/main/java/com/example/korucare/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.korucare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,22 +17,50 @@ class MainActivity : AppCompatActivity() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         val navHostFragment = findViewById<FragmentContainerView>(R.id.nav_host_fragment)
 
+        // Logo Change
+        val awaiLogo = findViewById<ImageView>(R.id.awaiLogo)
+        val korucareLogo = findViewById<ConstraintLayout>(R.id.korucareLogo)
+
+        // Default Values
+        awaiLogo.visibility = View.VISIBLE
+        korucareLogo.visibility = View.GONE
+
+
+        fun switchLogoVisibility(showKorucareLogo: Boolean) {
+            if (showKorucareLogo) {
+                awaiLogo.visibility = View.GONE
+                korucareLogo.visibility = View.VISIBLE
+            } else {
+                awaiLogo.visibility = View.VISIBLE
+                korucareLogo.visibility = View.GONE
+            }
+        }
+
         // Bot Nav
         bottomNav.setOnItemSelectedListener {
             item -> when(item.itemId) {
 
                 R.id.homeFragment -> {
                     navHostFragment.findNavController().navigate(R.id.homeFragment)
+                    switchLogoVisibility(false)
                     true
                 }
 
                 R.id.searchFragment -> {
                     navHostFragment.findNavController().navigate(R.id.searchFragment)
+                    switchLogoVisibility(false)
+                    true
+                }
+
+                R.id.infoFragment -> {
+                    navHostFragment.findNavController().navigate(R.id.infoFragment)
+                    switchLogoVisibility(true)
                     true
                 }
 
                 else -> false
             }
         }
+
     }
 }
